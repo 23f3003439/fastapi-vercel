51455143e8f3936ca42c84004e99d0729f70aa85
Saloni Puri <23f3003439@ds.study.iitm.ac.in>
2026-01-28 18:57:53 +0530

    Enhance CORS configuration for all origins and methods

--- api/index.py
+++ api/index.py
@@ -12,8 +12,10 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["POST"],
+    allow_credentials=False,
+    allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 # Load telemetry data
